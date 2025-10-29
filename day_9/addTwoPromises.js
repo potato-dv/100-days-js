66341afd06012d9f00d47d3a08fcd5485d6e3ae2
Lauris Lorenzo <Laurisjay@gmail.com>
2025-10-29 14:40:29 +0800

    Day 9: Add Two Promises-fix: added notes.

--- day_9/addTwoPromises.js
+++ day_9/addTwoPromises.js
@@ -4,13 +4,18 @@
  * promise1 and promise2 will both resolve with a number. The returned promise 
  * should resolve with the sum of the two numbers.
  */
-var addTwoPromises = async function(promise1, promise2) {
-    const [a, b] = await Promise.all([promise1, promise2]);
-    return a + b;
-};
 
-// Example usage:
-addTwoPromises(
-  new Promise(resolve => setTimeout(() => resolve(2), 20)),
-  new Promise(resolve => setTimeout(() => resolve(5), 60))
-).then(console.log); // Output: 7
+// async means your function always returns a "Promise".
+// await means wait for the "Promise" to finish before moving to the next line.
+
+var addTwoPromises = async function (promise1, promise2) { 
+  const [a, b] = await Promise.all([promise1, promise2]);
+    return a + b;
+  };
+
+  addTwoPromises(
+      new Promise(resolve => setTimeout(() => resolve(2), 2000)),
+      new Promise(resolve => setTimeout(() => resolve(5), 1000)))
+      .then(console.log); // 7
+
+
