32500e48e38d7c33c014b07d7d63dba8bc669504
Lauris Lorenzo <laurisjay@gmail.com>
2026-07-19 07:05:50 +0800

    Day 29:

---
+++ day-28/arrayMethod.js
@@ -0,0 +1,6 @@
+// Create a student-grade analyzer that calculates averages, passing students, and the highest scorer.
+
+function analyzeStudents(students) {
+    const totalGrades = students.reduce((sum, student) => sum + student.grade, 0);
+    
+}
