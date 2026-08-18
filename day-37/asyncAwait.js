ed444faa94405e2f331b45964663475244a5a7df
Lauris Lorenzo <laurisjay@gmail.com>
2026-08-18 23:14:35 +0800

    Day 37: add notes to day 37

--- day-37/asyncAwait.js
+++ day-37/asyncAwait.js
@@ -34,12 +34,24 @@
 
 
 async function fetchUser(id) {
+
+    // .find is a method that returns the first element in an array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned. 
+//     .find()
+// → searches the array
+// → checks each item
+// → returns the FIRST matching item
+// → stops when it finds one
+// → returns undefined if nothing matches
     const findUser = users.find(user => user.id === id);
     return findUser;
 }
-
-async function getActiveUsers(ids) {
+// this is connected to the started code.
+// .map works by creating a new array and It collects the return value of fetchUser() for each ID.
+async function activeUsers(ids) {
     const userPromises = ids.map(id => fetchUser(id));
+    
+    // await Promise.all() is used to wait for all the promises in the userPromises array to resolve. 
+    // It returns an array of resolved values (users) once all promises are fulfilled.
     const users = await Promise.all(userPromises);
     return users.filter(user => user.active);
 }
@@ -51,5 +63,5 @@ const users = [
     { id: 3, name: "Carl", active: true }
 ];
 
-getActiveUsers ([1, 2, 3]) 
+activeUsers ([1, 2, 3]) 
 .then(usersActive => console.log(usersActive));
