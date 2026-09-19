706d4650e9974e222460c9bb777f97804042d7cd
Lauris Lorenzo <laurisjay@gmail.com>
2026-09-19 21:49:05 +0800

    Day 58: Intro to Express

--- day-58/src/index.js
+++ day-58/src/index.js
@@ -1,5 +1,5 @@
-import { PORT } from "../config/config.js";
-import { server } from "../server/server.js";
+import { PORT } from "./config/config.js";
+import { server } from "./server/server.js";
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
